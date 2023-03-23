b92f41a6da2d39a430042d04193fd23ce6028094
Derek Foundoulis <derek@foundoul.is>
2023-03-23 13:04:03 -0600

    Adding repl.

--- src/main-repl.rs
+++ src/main-repl.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use boxr::lexer::lex;
+use linefeed::Interface;
+
+fn main() {
+    let reader = Interface::new("boxr").unwrap();
+    reader.set_prompt("==> ").unwrap();
+    loop {
+        match reader.read_line().unwrap() {
+            linefeed::ReadResult::Input(line) => {
+                let lexer_raw = lex(line);
+                println!("{:?}", lexer_raw);
+            }
+            linefeed::ReadResult::Eof => break,
+            linefeed::ReadResult::Signal(_) => break,
+        }
+    }
+}
