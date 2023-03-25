35742a252880303d8842424a49966e4fca843cf4
Derek Foundoulis <derek@foundoul.is>
2023-03-25 14:08:10 -0600

    Adding parsing tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,77 @@
 pub mod evaluator;
+pub mod scope;
 pub mod types;
+
+use lalrpop_util::lalrpop_mod;
+lalrpop_mod!(pub slyther);
+
+#[cfg(test)]
+mod test_parsing {
+    use std::sync::Arc;
+
+    use super::slyther::ExprsParser;
+    use super::types::{Expr, Value};
+
+    #[test]
+    fn test_parsing() {
+        let input = "(+ 1 2)";
+        let parsed_input = ExprsParser::new().parse(input);
+        assert!(parsed_input.is_ok());
+        let parsed_input = parsed_input.unwrap();
+        assert_eq!(parsed_input.len(), 1);
+        let expr = &parsed_input[0];
+        assert!(
+            expr == &Arc::new(Expr::List(vec![
+                Arc::new(Expr::Value(Value::Symbol("+".to_string()))),
+                Arc::new(Expr::Value(Value::Int(1))),
+                Arc::new(Expr::Value(Value::Int(2))),
+            ]))
+        );
+    }
+
+    #[test]
+    fn test_parsing_nested() {
+        let input = "(+ 1 (+ 2 3))";
+        let parsed_input = ExprsParser::new().parse(input);
+        assert!(parsed_input.is_ok());
+        let parsed_input = parsed_input.unwrap();
+        assert_eq!(parsed_input.len(), 1);
+        let expr = &parsed_input[0];
+        assert!(
+            expr == &Arc::new(Expr::List(vec![
+                Arc::new(Expr::Value(Value::Symbol("+".to_string()))),
+                Arc::new(Expr::Value(Value::Int(1))),
+                Arc::new(Expr::List(vec![
+                    Arc::new(Expr::Value(Value::Symbol("+".to_string()))),
+                    Arc::new(Expr::Value(Value::Int(2))),
+                    Arc::new(Expr::Value(Value::Int(3))),
+                ])),
+            ]))
+        );
+    }
+}
+
+#[cfg(test)]
+mod test_var_storage {
+    use super::scope::LexicalVarStorage;
+    use super::types::Value;
+
+    #[test]
+    fn test_set_get() {
+        let mut storage = LexicalVarStorage::new();
+        storage.put("a", Value::Int(1));
+        assert_eq!(storage.get("a"), Some(&Value::Int(1)));
+    }
+
+    #[test]
+    fn test_fork() {
+        let mut storage = LexicalVarStorage::new();
+        storage.put("a", Value::Int(1));
+        let mut fork = storage.fork();
+        fork.put("b", Value::Int(2));
+        assert_eq!(storage.get("a"), Some(&Value::Int(1)));
+        assert_eq!(storage.get("b"), None);
+        assert_eq!(fork.get("a"), Some(&Value::Int(1)));
+        assert_eq!(fork.get("b"), Some(&Value::Int(2)));
+    }
+}
