c65c80df134c0e65b80752f65fd30b1108bf75ad
Derek Foundoulis <derek@foundoul.is>
2023-03-29 16:12:42 -0600

    Adding var define and access test.

--- src/lib.rs
+++ src/lib.rs
@@ -483,4 +483,27 @@ mod test_builtin_macro {
         assert!(result.is_ok());
         assert_eq!(storage.get("a"), Some(&Expr::Value(Value::Int(1))));
     }
+
+    #[test]
+    fn test_define_var_access_var() {
+        let mut storage = LexicalVarStorage::new();
+        let input = "(define a 1) a";
+        let parsed_input = ExprsParser::new().parse(input);
+        assert!(parsed_input.is_ok());
+        let parsed_input = parsed_input.unwrap();
+        assert_eq!(parsed_input.len(), 2);
+
+        let results = parsed_input
+            .iter()
+            .map(|expr| lisp_eval(expr, &mut storage))
+            .collect::<Vec<_>>();
+        assert!(results.iter().all(|result| result.is_ok()));
+        let results = results
+            .into_iter()
+            .map(|result| result.unwrap())
+            .collect::<Vec<_>>();
+        assert_eq!(results.len(), 2);
+        assert_eq!(results[0], Expr::Value(Value::NIL));
+        assert_eq!(results[1], Expr::Value(Value::Int(1)));
+    }
 }
